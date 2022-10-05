1d4559bc791a98026037c833218f0840aafd3925
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-10-05 14:27:59 -0300

    Ok

--- game.java
+++ game.java
@@ -15,10 +15,10 @@ import java.util.Scanner;
       int user_choice;
 
       show.println("Hello, whats your name?");
-      String nome = scan.nextLine();
-      show.println("Nice meet you"+ nome+"!");
-      show.println( nome+"Do you play whit me?");
-      show.println("\nSelect one number.\n 1 -> 2 ");
+      String name = scan.nextLine();
+      show.println("Nice meet you"+name+"!");
+      show.println( name+"Do you play whit me?");
+      show.println("\nSelect one number.\n 1 -> YES 2 -> NOT");
       op = scan.nextInt();
 
       if(op == 2){
@@ -26,6 +26,23 @@ import java.util.Scanner;
       }else if(op == 1){
         show.println("OK! THE RULES OF THE GAME");
         show.println("I choose a number between 0 and 10");
+        show.println("But, i dont tell you what number i will chose");
+        show.println("Try to guess the number. If you choose the correct number you are the winner");
+
+        pc_choice = rand.nextInt(11);
+
+        show.println("OKK!"+name+"I already chose");
+        show.println("Now!Try to guess");
+
+        do{
+          user_choice = scan.nextInt();
+          if(user_choice != pc_choice){
+            show.println("wrong ansewr");
+          }else{
+            show.println("OMG!! IS CORRECT");
+          }
+        }while(pc_choice != user_choice);
+
       }
 
     }
